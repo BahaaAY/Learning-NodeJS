2f09ace9e3895864d29bc734704f405bd942c6aa
BahaaAY <bahaa.abouyassine2@gmail.com>
2023-07-18 22:26:55 +0300

    taking requests and sending response!!

--- node-js-basics/app.js
+++ node-js-basics/app.js
@@ -8,6 +8,17 @@ const http = require('http'); //importing the http library
 
 
 const server = http.createServer((req,res)=>{
-console.log(req);
+console.log(req.url, req.method, req.headers);
+console.log("url: " + req.url +"\n method: " + req.method)
+//process.exit(); stops the event loop
+
+res.setHeader('Content-Type' , 'text/html');
+res.write('<html>');
+res.write('<head><title>My First Server Response!</title></head>');
+res.write('<body><h1>Hello World From My Node.js Server!!!</h1></body>');
+res.write('</html>');
+res.end();
+
+
 });
 server.listen(3000);
