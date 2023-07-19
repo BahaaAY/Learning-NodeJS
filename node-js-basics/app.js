e6ac14d8f518cc2ada7a0fd3da75167e13ae000e
BahaaAY <bahaa.abouyassine2@gmail.com>
2023-07-19 11:43:28 +0300

    routes as a module

--- node-js-basics/app.js
+++ node-js-basics/app.js
@@ -1,51 +1,10 @@
 const http = require('http'); //importing the http library
-const fs = require('fs');
 
 /**function httpListener(req, res) // httpListener(request, response)
 {
 
 }**/
 
-
-const server = http.createServer((req,res)=>{
-//console.log(req.url, req.method, req.headers);
-//console.log("url: " + req.url +"\n method: " + req.method)
-//process.exit(); stops the event loop
-if(req.url === '/')
-{
-    res.setHeader('Content-Type' , 'text/html');
-res.write('<html>');
-res.write('<head><title>My First Server Response!</title></head>');
-res.write('<body><h1>Welcome</h1> <form action= "/message"method="POST"><input name="message" type="text"><button type="submit">Send</button></form></body>');
-res.write('</html>');
-return res.end();
-}
-if(req.url === '/message' && req.method === 'POST')
-{
-    const body =[];
-    req.on('data', (chunk)=>{
-        body.push(chunk);
-    });
-    req.on('end',()=>{
-        const parsedBody = Buffer.concat(body).toString();
-        const message = parsedBody.split('=')[1];
-        fs.writeFileSync('messages',message);
-
-    });
-    
-    
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
-
-}
-res.setHeader('Content-Type' , 'text/html');
-res.write('<html>');
-res.write('<head><title>My First Server Response!</title></head>');
-res.write('<body><h1>Hello World From My Node.js Server!!!</h1></body>');
-res.write('</html>');
-res.end();
-
-
-});
+const routes = require('./routes');
+const server = http.createServer(routes.requestHandler);
 server.listen(3000);
