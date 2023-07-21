3c89cf3adaa8810169d3033842db5be724cb4b28
BahaaAY <bahaa.abouyassine2@gmail.com>
2023-07-21 14:17:42 +0300

    Serving HTML files with static css files

--- express-js/app.js
+++ express-js/app.js
@@ -1,17 +1,25 @@
 
+const path = require('path');
+
 const express = require('express');
 const bodyParser =require('body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const rootDir = require('./util/path');
 
 const app = express();
 
 app.listen(3000);
 
 app.use(bodyParser.urlencoded({extended :false}));
+app.use(express.static(path.join(rootDir,'public')));
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 
-app.use(shopRoutes);
+app.use(shopRoutes.router);
+
+app.use((req,res,next)=>{
+res.status(404).sendFile(path.join(rootDir,'views','404.html'));
+});
 
--- express-js/routes/shop.js
+++ express-js/routes/shop.js
@@ -1,10 +1,17 @@
+const path = require('path');
+
+
 const express = require('express');
 
 const router = express.Router();
 
+const rootDir = require('./../util/path');
+
+
+
 
 router.get('/',(req,res,next)=>{
-    res.send('<h1>Welcome!</h1>');
+    res.sendFile(path.join(rootDir,'views','shop.html'));
 });
 
 
