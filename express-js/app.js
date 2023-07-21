59d6de734feca6383a1911679bbfc0af04baa7c6
BahaaAY <bahaa.abouyassine2@gmail.com>
2023-07-21 08:41:10 +0300

    using express router

--- express-js/app.js
+++ express-js/app.js
@@ -1,26 +1,17 @@
-const http = require('http');
 
 const express = require('express');
 const bodyParser =require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.listen(3000);
 
 app.use(bodyParser.urlencoded({extended :false}));
-app.use('/add-product', (req,res,next)=>{
 
-    res.send('<form method="POST" action="/product"><input type="text" name="title"/><button type="submit">Save</button></form>');
+app.use(adminRoutes);
 
-});
+app.use(shopRoutes);
 
-app.post('/product',(req,res,next)=>{
-
-    console.log(req.body);          
-    res.redirect('/');
-});
-
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Welcome!</h1>');
-});
