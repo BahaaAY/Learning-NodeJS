4ab0b751cc7294c3cf0a874508e23020d13262f2
BahaaAY <bahaa.abouyassine2@gmail.com>
2023-07-23 06:43:17 +0300

    Project Init, Adding Pug Templating engine

---
+++ Shop/app.js
@@ -0,0 +1,27 @@
+
+const path = require('path');
+
+const express = require('express');
+const bodyParser =require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const rootDir = require('./util/path');
+
+const app = express();
+app.set('view engine','pug');
+app.listen(3000);
+
+app.use(bodyParser.urlencoded({extended :false}));
+app.use(express.static(path.join(rootDir,'public')));
+
+app.use('/admin',adminRoutes);
+
+app.use(shopRoutes.router);
+
+
+
+app.use((req,res,next)=>{
+res.status(404).sendFile(path.join(rootDir,'views','404.html'));
+});
+
